092701ad94f961a2290c1694df4d6308b4401632
cringuxd <193437229+cringuxd@users.noreply.github.com>
2025-03-11 13:00:21 -0500

    Set up createGrid() and deleteGrid()
    Alongside these changes are a few style changes to the sketch pad, including making the grid pad itself square.
    The createGrid() function utilizes flex-wrap and automatic calculation of width and height to make the pad functional.
    the deleteGrid() function will be used later on when adding buttons to change the number of squares in the grid, as well as resetting any color changes to the grid.

--- code.js
+++ code.js
@@ -1,18 +1,33 @@
 //Divide the x by x grid by creating x "row" containers, each containing x squares
+const grid = document.querySelector(".grid")
+let rowList = [];
 
 
 //Start with 16 containers as a default (use node list)
-function createGrid(inputValue) {
-    let rowList = [];
+function createGrid(inputValue, rowList) {
     let gridSquares = inputValue * inputValue;
-
+    let squareSize = calculateSquareSize(inputValue);
     for(let i = 0; i < gridSquares; i++) {
         rowList[i] = document.createElement("div");
         rowList[i].setAttribute("class","square");
-        container.appendChild(rowList[i]);
+        rowList[i].style.width = squareSize.toString() +"%";
+        rowList[i].style.height = squareSize.toString() +"%";
+        console.log (rowList[i].style.width);
+        grid.appendChild(rowList[i]);
+    }
+}
+//Loops through the grid container and removes each child starting from the last, resets rowList
+function deleteGrid() {
+    rowList = []
+    while(grid.lastElementChild) {
+        grid.removeChild(grid.lastElementChild);
     }
 }
 
-//createGrid(16);
+function calculateSquareSize(n) {
+    return Math.round((1/n)*100);
+}
 
-//function deleteGrid() {}
+createGrid(10, rowList);
+
+
